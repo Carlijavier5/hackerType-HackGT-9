2916ea627c90d3ff55109b9178dd97e07cde9580
Carlos Suarez Barrera <carlijavier5@gmail.com>
2022-10-22 19:36:53 -0400

    python strings revamped

--- script-code.js
+++ script-code.js
@@ -1,7 +1,12 @@
-const items = ["#include <iostream>\nusing namespace std;\nint main() {\nint a;\nint b;\ncin>>a>>b;\ncout<<a+b;\nreturn 0;\n}",
+const items = ["Python!",
+              "num = 8\nnum_sqrt = num ** 0.5\nprint('The square root of %0.3f is %0.3f'%(num ,num_sqrt))",
+              "a = 5\nb = 6\nc = 7\n\ns = (a + b + c) / 2\n\narea = (s*(s-a)*(s-b)*(s-c)) ** 0.5",
+              "my_list = [1, 2, 3, 4, 5]\nfor elem in my_list:\n\tif elem % 2 == 0:\n\t\tprint(\"Even:\", elem)\n\t\tprint(\"break\")\n\t\tbreak\n \telse:\n\t\tprint(\"Odd:\", elem)",
+              "C++!",
+              "#include <iostream>\nusing namespace std;\nint main() {\nint a;\nint b;\ncin>>a>>b;\ncout<<a+b;\nreturn 0;\n}",
               "#include <iostream>\nusing namespace std;\nint main() {\nint a;\ncin>>a;\nif(a%2 == 0)\ncout<<\"even\";\nelse cout<<\"odd\";\nreturn 0;\n}",
               "#include <iostream>\nusing namespace std;\nint main() {\nstring str;\ncin>>str;\nint count = 0;\nfor(int i = 0;str[i];i++) count++;\ncout<<count;",
-              "my_list = [1, 2, 3, 4, 5]\nfor elem in my_list:\n\tif elem % 2 == 0:\n\t\tprint(\"Even:\", elem)\n\t\tprint(\"break\")\n\t\tbreak\n \telse:\n\t\tprint(\"Odd:\", elem)"];
+              ];
 var i = -1;
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
