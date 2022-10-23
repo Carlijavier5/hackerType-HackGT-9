8d14d2e2d0c0c512e9a42f7fc62b49c59cdfea5c
brateepodder <114537684+brateepodder@users.noreply.github.com>
2022-10-22 22:27:49 -0400

    Updated but don't use this QuoteTyperScript file

--- QuoteTyperScript(ignore).js
+++ QuoteTyperScript(ignore).js
@@ -2,18 +2,13 @@ const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
-const userTyped = new Event("waitType")
-const stopCount = new Event("stopCounting")
 
-renderNewQuote()
+renderNewQuote() 
 
 quoteInputElement.addEventListener('input', () => {
   const arrayQuote = quoteDisplayElement.querySelectorAll('span')
   const arrayValue = quoteInputElement.value.split('')
-  document.getElementById("timer").style.color='#FFFFFF';
-  if (!started) {
-    document.dispatchEvent(userTyped)
-  }
+  //let correctcharacter = true
   let correct = true
   arrayQuote.forEach((characterSpan, index) => {
     const character = arrayValue[index]
@@ -29,12 +24,16 @@ quoteInputElement.addEventListener('input', () => {
     } else {
       characterSpan.classList.remove('correct')
       characterSpan.classList.add('incorrect')
+      correctcharacter = false
+      //while (correctcharacter === false) { 
+        //quoteInputElement.addEventListener('input', () => 
+        
+     // }           
     }
   })
 
   if (correct) { 
     renderNewQuote()
-    document.dispatchEvent(stopCount)
   } 
 })
 
@@ -45,12 +44,7 @@ function getRandomQuote() {
 }
 
 async function renderNewQuote() {
-  timerElement.innerText = 0
-  document.getElementById("timer").style.color="#222222";
-  document.dispatchEvent(stopCount)
-  started = false
-  startTimer()
-  const quote = await getRandomQuote() 
+  const quote = await getRandomQuote()
   quoteDisplayElement.innerHTML = ''
   quote.split('').forEach(character => { // for looping through each character 
     const characterSpan = document.createElement('span')
@@ -58,28 +52,20 @@ async function renderNewQuote() {
     quoteDisplayElement.appendChild(characterSpan)
   })
   quoteInputElement.value = null
+  startTimer()
 }
-
 
 let startTime
 function startTimer() {
-timerElement.innerText = 0
-started = false
-document.addEventListener("waitType", e => {
-  started = true
+  timerElement.innerText = 0
   startTime = new Date()
   setInterval(() => {
     timer.innerText = getTimerTime()
   }, 1000)
-})
-
-document.addEventListener("stopCounting", e => {
-  return
-})
-
 }
 
 function getTimerTime() {
   return Math.floor((new Date() - startTime) / 1000)
 }
+
 
